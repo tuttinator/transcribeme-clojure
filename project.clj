668c0f5bf58e9146ccf22239a4380e84a7741ac0
Caleb Tutty <caleb@prettymint.co.nz>
2013-10-14 11:45:08 +1300

    Updated the licence url for MIT licence

--- project.clj
+++ project.clj
@@ -2,5 +2,5 @@
   :description "A library for interacting with the TranscribeMe SOAP API"
   :url "https://github.com/tuttinator/transcribeme-clojure"
   :license {:name "MIT"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
